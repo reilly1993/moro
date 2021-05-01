82cca67194cba7feee20885371a2176b780feb44
martin <mail@mama.sh>
2021-05-01 12:24:27 +0200

    logo change

--- src/App.js
+++ src/App.js
@@ -89,12 +89,12 @@ const Private = () => {
                   <div className="flex-shrink-0 flex items-center">
                     <img
                       className="block lg:hidden h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                      src="https://mama.sh/logo.svg"
                       alt="Workflow"
                     />
                     <img
                       className="hidden lg:block h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                      src="https://mama.sh/logo.svg"
                       alt="Workflow"
                     />
                   </div>
