b77d296bbb7dfa8c0ef2cd68115ca19e3b5b9c9a
martin <mail@mama.sh>
2021-05-01 12:20:17 +0200

    logo change

--- src/SignIn.js
+++ src/SignIn.js
@@ -16,12 +16,12 @@ export default function SignIn() {
       <div className="max-w-md w-full space-y-8">
         <div>
           <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            className="mx-auto h-18 w-auto"
+            src="https://mama.sh/logo.svg"
             alt="Workflow"
           />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+              Admin side
           </h2>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
