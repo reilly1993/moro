f5f7d2dbf484e1bdcc1eb0ec05687da004111923
martin <mail@mama.sh>
2021-05-01 12:12:28 +0200

    moro colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "moro-dark-blue": "#002384",
+        "moro-dark-green": "#033730",
+        "moro-dark-orange": "#033730",
+        "moro-dark-red": "#990000",
+        "moro-dark-purple": "#260023",
+        "moro-dark-yellow": "#ffa500",
+        "moro-purple": "#d3caee",
+        "moro-green": "#d7ffd7",
+        "moro-red": "#ffb7fc",
+        "moro-blue": "#D5E3F2",
+        "moro-orange": "#ffe8dc",
+        "moro-yellow": "#fffdba",
+      },
+    },
   },
   variants: {
     extend: {},
