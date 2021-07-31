08266db78ce209e46a88db241b1f8f5788373163
Christian Reilly Vingborg <christian@weld.app>
2021-07-31 11:44:09 +0200

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
-export const SERVER_URL = process.env.REACT_APP_SERVER_URL || "localhost:8080";
+export const SERVER_URL =
+  process.env.REACT_APP_SERVER_URL || "http://localhost:8080";
 
 //https://api.moro.mama.sh
